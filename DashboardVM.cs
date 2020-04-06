cc4884080f4c1460970d65a8c4c6f82b31f63a1e
LalivT <59091636+LalivT@users.noreply.github.com>
2020-04-06 20:43:23 +0300

    Add files via upload

---
+++ DashboardVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace FlightApp
+{
+    public class DashboardVM : INotifyPropertyChanged
+    {
+        private IFlightAppModel model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DashboardVM(IFlightAppModel model)
+        {
+            this.model = model;
+            model.PropertyChanged +=
+                delegate (Object sender, PropertyChangedEventArgs e)
+                {
+                    NotifyPropertyChanged("VM_" + e.PropertyName);
+                };
+        }
+
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public double VM_heading_deg
+        {
+            get { return model.heading_deg; }
+        }
+    }
+}
---
+++ IFlightAppModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace FlightApp
+{
+    public interface IFlightAppModel : INotifyPropertyChanged
+    {
+        //event PropertyChangedEventHandler PropertyChanged;
+        void NotifyPropertyChanged(string propName);
+        void connect(string ip, int port);
+        void disconnect();
+
+        void start();
+
+        double heading_deg { get; set;}
+
+
+    }
+}
